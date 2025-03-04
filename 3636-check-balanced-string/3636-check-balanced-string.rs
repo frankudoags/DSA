fcc57fd8a93b327784fff363b54f536159c0e799
Udoagwa Franklin <54338168+frankudoags@users.noreply.github.com>
2025-03-04 16:21:50 +0100

    Time: 0 ms (100%), Space: 2.2 MB (55%) - LeetHub

--- 3636-check-balanced-string/3636-check-balanced-string.rs
+++ 3636-check-balanced-string/3636-check-balanced-string.rs
@@ -2,8 +2,8 @@ impl Solution {
     pub fn is_balanced(num: String) -> bool {
         let (mut even_sum, mut odd_sum) = (0, 0);
 
-        for (i, ch) in num.into_bytes().into_iter().enumerate() {
-            let val = ch - b'0';
+        for (i, ch) in num.chars().enumerate() {
+            let val = ch.to_digit(10).unwrap_or(0);
             match i % 2 {
                 0 => even_sum += val,
                 _ => odd_sum += val,
